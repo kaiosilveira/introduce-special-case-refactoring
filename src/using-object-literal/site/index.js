94ba00b17dca983d9dee007ea59a87369418369a
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-13 21:05:07 +0100

    using-obj-literal: return unknown customer from factory at `Site.customer`

--- src/using-object-literal/site/index.js
+++ src/using-object-literal/site/index.js
@@ -1,9 +1,11 @@
+import { createUnknownCustomer } from '../customer';
+
 export class Site {
   constructor({ customer }) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === 'unknown' ? createUnknownCustomer() : this._customer;
   }
 }
--- src/using-object-literal/site/index.test.js
+++ src/using-object-literal/site/index.test.js
@@ -5,4 +5,9 @@ describe('Site', () => {
     const aSite = new Site({ customer: { name: 'John Doe' } });
     expect(aSite.customer).toEqual({ name: 'John Doe' });
   });
+
+  it('should return an unknown customer if the customer is unknown', () => {
+    const aSite = new Site({ customer: 'unknown' });
+    expect(aSite.customer.isUnknown).toBe(true);
+  });
 });

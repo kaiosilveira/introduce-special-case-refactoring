c6cd32c1e427c3c834fbacb550828f16be535a77
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-13 14:39:02 +0100

    using-a-class: return a `UnknownCustomer` as special case at `Site`

--- src/using-a-class/site/index.js
+++ src/using-a-class/site/index.js
@@ -1,9 +1,11 @@
+import { UnknownCustomer } from '../customer';
+
 export class Site {
   constructor({ customer }) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === 'unknown' ? new UnknownCustomer() : this._customer;
   }
 }
--- src/using-a-class/site/index.test.js
+++ src/using-a-class/site/index.test.js
@@ -1,8 +1,14 @@
 import { Site } from '.';
+import { UnknownCustomer } from '../customer';
 
 describe('Site', () => {
   it('should have a customer', () => {
     const aSite = new Site({ customer: { name: 'John Doe' } });
     expect(aSite.customer).toEqual({ name: 'John Doe' });
+  });
+
+  it('should return an unknown customer if no customer is provided', () => {
+    const aSite = new Site({ customer: 'unknown' });
+    expect(aSite.customer).toBeInstanceOf(UnknownCustomer);
   });
 });
